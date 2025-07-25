af2c222547bb20d6605188066f5120290ee7a1df
TomRegnier <tomregnier83@gmail.com>
2025-07-25 16:03:25 +0200

    New commit 25/07

--- script.js
+++ script.js
@@ -155,80 +155,15 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+function telechargerCV() {
+  const lien = document.createElement('a');
+  lien.href = 'cv.pdf'; // Remplace par le chemin correct vers ton CV
+  lien.download = 'Mon_CV.pdf'; // Nom du fichier une fois téléchargé
+  document.body.appendChild(lien);
+  lien.click();
+  document.body.removeChild(lien);
+}
 
-// Who i am partie
-
-// secondepage
-
-// const letters = [
-//   document.getElementById("DESIGND"),
-//   document.getElementById("DESIGNE"),
-//   document.getElementById("DESIGNS"),
-//   document.getElementById("DESIGNI"),
-//   document.getElementById("DESIGNG"),
-//   document.getElementById("DESIGNN"),
-// ];
-
-// function animationDesign() {
-//   letters.forEach((letter, index) => {
-//     setTimeout(() => {
-//       letter.classList.add("scale-up");
-//       setTimeout(() => {
-//         letter.classList.remove("scale-up");
-//       }, 800);
-//     }, index * 200);
-//   });
-// }
-
-// setInterval(animationDesign, 2000);
-
-// Animation des lettre pour Une passion dans design
-
-// const passionletter = [
-//   // document.getElementById("letter1"), // U
-//   // document.getElementById("letter2"), // N
-//   // document.getElementById("letter3"), // E
-//   document.getElementById("letter4"), // &nbsp
-//   document.getElementById("letter5"), // P
-//   document.getElementById("letter6"), // A
-//   document.getElementById("letter7"), // S
-//   document.getElementById("letter8"), // S
-//   document.getElementById("letter9"), // I
-//   document.getElementById("letter10"), // O
-//   document.getElementById("letter11"), // N
-// ];
-
-// function animationPassion() {
-//   passionletter.forEach((passionletter, index) => {
-//     setTimeout(() => {
-//       passionletter.classList.add("scale-up");
-//       setTimeout(() => {
-//         passionletter.classList.remove("scale-up");
-//       }, 700);
-//     }, index * 170);
-//   })
-// }
-
-// setInterval(animationPassion, 2000);
-
-// Animation des lettre pour Une passion dans design
-
-
-// Machine a écrire sur developpement 
-
-// const devtext = document.getElementById("titledev")
-
-// let k = 0;
-// const textdev = "DÉVELOPPEMENT";
-
-// function devwriter() {
-//   if (k < textdev.length) {
-//     devtext.textContent = textdev.substring(0, k);
-//     k++;
-//     setTimeout(devwriter, 150);
-//   }
-// }
-
-// devwriter()
-
-// Paragraphe code
+function viewCv() {
+  window.open("/cv.pdf", "_blank");
+}
